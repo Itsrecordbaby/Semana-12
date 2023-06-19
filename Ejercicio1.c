4b872c371dd0a6b2c433a116083eabc2a57efb8a
Mati <amaticc03@gmail.com>
2023-06-19 15:41:19 -0500

    tercero

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -24,7 +24,7 @@ int main() {
     printf("Ingrese el promedio del alumno: ");
     scanf("%f", &a1.promedio);
 
-    printf("\nDatos del alumno:\n");
+    printf("\nDatos del primer alumno:\n");
     puts(a1.nombre);
     puts(a1.carrera);
     puts(a1.direccion);
@@ -33,13 +33,13 @@ int main() {
 
 struct alumno a2;
 
-    printf("Ingrese el nombre del alumno: ");
+    printf("\nIngrese el nombre del alumno: ");
     gets(a2.nombre);
 
     printf("Ingrese la carrera del alumno: ");
     gets(a2.carrera);
 
-    printf("Ingrese la dirección del alumno: ");
+    printf("Ingrese la direccion del alumno: ");
     gets(a2.direccion);
 
     printf("Ingrese la edad del alumno: ");
@@ -48,7 +48,7 @@ struct alumno a2;
     printf("Ingrese el promedio del alumno: ");
     scanf("%f", &a2.promedio);
 
-    printf("\nDatos del alumno:\n");
+    printf("\nDatos del segundo alumno:\n");
     puts(a2.nombre);
     puts(a2.carrera);
     puts(a2.direccion);
@@ -57,13 +57,13 @@ struct alumno a2;
 
     struct alumno a3;
 
-    printf("Ingrese el nombre del alumno: ");
+    printf("\nIngrese el nombre del alumno: ");
     gets(a3.nombre);
 
     printf("Ingrese la carrera del alumno: ");
     gets(a3.carrera);
 
-    printf("Ingrese la dirección del alumno: ");
+    printf("Ingrese la direccion del alumno: ");
     gets(a3.direccion);
 
     printf("Ingrese la edad del alumno: ");
@@ -72,7 +72,7 @@ struct alumno a2;
     printf("Ingrese el promedio del alumno: ");
     scanf("%f", &a3.promedio);
 
-    printf("\nDatos del alumno:\n");
+    printf("\nDatos del tercer alumno:\n");
     puts(a3.nombre);
     puts(a3.carrera);
     puts(a3.direccion);
@@ -81,13 +81,13 @@ struct alumno a2;
 
     struct alumno a4;
 
-    printf("Ingrese el nombre del alumno: ");
+    printf("\nIngrese el nombre del alumno: ");
     gets(a4.nombre);
 
     printf("Ingrese la carrera del alumno: ");
     gets(a4.carrera);
 
-    printf("Ingrese la dirección del alumno: ");
+    printf("Ingrese la direccion del alumno: ");
     gets(a4.direccion);
 
     printf("Ingrese la edad del alumno: ");
@@ -96,7 +96,7 @@ struct alumno a2;
     printf("Ingrese el promedio del alumno: ");
     scanf("%f", &a4.promedio);
 
-    printf("\nDatos del alumno:\n");
+    printf("\nDatos del cuarto alumno:\n");
     puts(a4.nombre);
     puts(a4.carrera);
     puts(a4.direccion);
@@ -105,13 +105,13 @@ struct alumno a2;
 
     struct alumno a5;
 
-    printf("Ingrese el nombre del alumno: ");
+    printf("\nIngrese el nombre del alumno: ");
     gets(a5.nombre);
 
     printf("Ingrese la carrera del alumno: ");
     gets(a5.carrera);
 
-    printf("Ingrese la dirección del alumno: ");
+    printf("Ingrese la direccion del alumno: ");
     gets(a5.direccion);
 
     printf("Ingrese la edad del alumno: ");
@@ -120,7 +120,7 @@ struct alumno a2;
     printf("Ingrese el promedio del alumno: ");
     scanf("%f", &a5.promedio);
 
-    printf("\nDatos del alumno:\n");
+    printf("\nDatos del quinto alumno:\n");
     puts(a5.nombre);
     puts(a5.carrera);
     puts(a5.direccion);
